edbddea2ca9380a50b5e79d74ac112e1dbf32a91
Ayfri <pierre.ayfri@gmail.com>
2021-06-01 16:00:30 +0200

    feat: Add kord-extensions dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+val kordexVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.4.32"
@@ -9,15 +10,22 @@ group = "fr.ayfri"
 version = "0.1"
 
 repositories {
-    mavenCentral()
+    maven {
+        name = "Kotlin Discord"
+        url = uri("https://maven.kotlindiscord.com/repository/maven-public/")
+    }
+}
+
+dependencies {
+    implementation("com.kotlindiscord.kord.extensions:kord-extensions:$kordexVersion")
 }
 
 
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
 }
 
 application {
-    mainClassName = "MainKt"
+    mainClass.set("MainKT")
 }
