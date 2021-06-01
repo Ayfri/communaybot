30d3b445608551fb53b00de706d34d948a08bb4b
Ayfri <pierre.ayfri@gmail.com>
2021-06-01 16:23:46 +0200

    chore(deps): Add groovy dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val kordexVersion: String by project
 val logbackVersion: String by project
 val kotlinLoggingVersion: String by project
+val groovyVersion: String by project
+
 
 plugins {
     kotlin("jvm") version "1.4.32"
@@ -23,6 +25,7 @@ dependencies {
     implementation("com.kotlindiscord.kord.extensions:kord-extensions:$kordexVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
+    implementation("org.codehaus.groovy:groovy:$groovyVersion")
 }
 
 
