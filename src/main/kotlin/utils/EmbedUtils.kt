401e526d283847f2c5720d7d8e76536765afe3d0
Ayfri <pierre.ayfri@gmail.com>
2021-06-02 02:49:36 +0200

    feat: Add EmbedUtils.

---
+++ src/main/kotlin/utils/EmbedUtils.kt
@@ -0,0 +1,27 @@
+package utils
+
+import com.kotlindiscord.kord.extensions.ExtensibleBot
+import dev.kord.core.Kord
+import dev.kord.core.supplier.EntitySupplyStrategy
+import dev.kord.rest.builder.message.EmbedBuilder
+import java.time.Instant
+
+suspend fun basicEmbed(bot: ExtensibleBot) = basicEmbed(bot.getKoin().get<Kord>())
+suspend fun basicEmbed(client: Kord): suspend EmbedBuilder.() -> Unit = {
+	val user = client.getSelf(EntitySupplyStrategy.cacheWithRestFallback)
+	
+	footer {
+		icon = user.avatar.url
+		text = user.username
+	}
+	
+	timestamp = Instant.now()
+}
+
+suspend fun completeEmbed(bot: ExtensibleBot, title: String, description: String) = completeEmbed(bot.getKoin().get<Kord>(), title, description)
+suspend fun completeEmbed(client: Kord, title: String, description: String): suspend EmbedBuilder.() -> Unit = {
+	basicEmbed(client)()
+	
+	this.title = title
+	this.description = description
+}
